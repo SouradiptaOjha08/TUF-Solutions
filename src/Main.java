f54e382a17abeaa0977f377e91baf7f10dfff18f
SouradiptaOjha08 <souradipta2021@gmail.com>
2026-06-22 14:21:39 +0530

    added few pattern problems

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int number = scanner.nextInt();
         System.out.println(sumOfTheNumber(number));
 
+        scanner.close();
     }
 
     public static long sumOfTheNumber(int number) {
