72481f32305fe039d975ab7a04e50e1c55d2554e
SouradiptaOjha08 <souradipta2021@gmail.com>
2026-09-17 13:28:23 +0530

    PerfectNumber number code added

---
+++ src/basic_math/PerfectNumber.java
@@ -0,0 +1,22 @@
+package basic_math;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input= scanner.nextInt();
+       System.out.println(isPerfect(input));
+    }
+    public static boolean isPerfect(int n) {
+        int original=n;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum==original;
+    }
+}
+
