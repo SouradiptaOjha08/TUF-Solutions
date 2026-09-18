7b3f6276045499fb6773d579bd6d6a48541a2bc9
SouradiptaOjha08 <souradipta2021@gmail.com>
2026-09-18 06:50:24 +0530

    small change made in prime number code in basic_math

--- src/basic_math/PrimeNumber.java
+++ src/basic_math/PrimeNumber.java
@@ -11,7 +11,7 @@ public class PrimeNumber {
 
     private static boolean isPrime(int input) {
         if (input == 1) return false;
-        for (int i = 2; i * i < input; i++) {
+        for (int i = 2; i * i <=input; i++) {
             if (input % i == 0) {
                 return false;
             }
