65a085e0778c4b056f78ae797f5af3c28a95454e
SouradiptaOjha08 <souradipta2021@gmail.com>
2026-09-19 06:48:36 +0530

    Untrack IDE iml and temporary test files, update .gitignore

--- src/pattern_problems/test.java
+++
@@ -1,9 +0,0 @@
-package pattern_problems;
-
-public class test {
-    public static void main(String[] args) {
-        for (int i=1 ; i<10 ; i++){
-            System.out.println(i);
-        }
-    }
-}
